8fabbda11791bb05c80600032b40afa0697dde61
aurora-novis <michael.albada@gmail.com>
2024-10-30 23:21:48 -0700

    Checkin langgraph memory example

---
+++ langgraph/memory.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, MessagesState, START
+
+llm = ChatOpenAI(model="gpt-4o")
+
+
+def call_model(state: MessagesState):
+    response = llm.invoke(state["messages"])
+    return {"messages": response}
+
+
+builder = StateGraph(MessagesState)
+builder.add_node("call_model", call_model)
+builder.add_edge(START, "call_model")
+graph = builder.compile()
+
+# Fails to maintain state across the conversation
+input_message = {"type": "user", "content": "hi! I'm bob"}
+for chunk in graph.stream({"messages": [input_message]}, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+input_message = {"type": "user", "content": "what's my name?"}
+for chunk in graph.stream({"messages": [input_message]}, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+graph = builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
+input_message = {"type": "user", "content": "hi! I'm bob"}
+for chunk in graph.stream({"messages": [input_message]}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+
+
